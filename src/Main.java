a8b0780a6229a498dd408c3cfbbab6ffd2e98a71
DeF <DeF-2008@yandex.ru>
2023-09-13 10:37:02 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketCost = 13676;
+        int ticketCost = 14500;
         int bonusScale = 20;
         int bonusMile = ticketCost / bonusScale;
-
         System.out.println("При покупке билета стоимостью " + ticketCost + " руб., Ваш бонус составит " + bonusMile + " бонус миль.");
     }
 }
